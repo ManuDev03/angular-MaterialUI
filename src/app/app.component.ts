f1409bcc66ff3a3e1e198493d0ef22dd7598b50e
ManuDev03 <manuprajan03@gmail.com>
2021-08-03 13:00:54 +0530

    MaterialUI-Autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable,from} from 'rxjs'
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-material';
+  mydata = new FormControl();
+  filterdOption:Observable<string[]>= from([])
+  data:string[]=["One","two","three","four","five"]
+  
+  ngOnInit():void{
+    this.filterdOption = this.mydata.valueChanges.pipe(startWith(''),map((value)=>this.filterValues(value)))
+
+  }
+  filterValues(value: string): string[] {
+    let filterd_value = value.toLowerCase();
+
+    return this.data.filter(option => option.toLowerCase().includes(filterd_value));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule} from '@angular/forms'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatInputModule } from '@angular/material/input'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
